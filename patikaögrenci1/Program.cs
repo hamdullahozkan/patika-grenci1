c7008c8ffdeaf2ddb21836804eca60137eda2be1
hamdullahozkan <hamdullahozkann@gmail.com>
2024-12-18 22:56:35 +0300

    Proje dosyası ekle.

---
+++ patikaögrenci1/Program.cs
@@ -0,0 +1,41 @@
+﻿int a = 5;
+int b = 7;
+
+int sun = a + b;
+
+int diffrence = a - b;
+
+int product = a * b;
+
+int quotient = a / b;
+
+int remainder = a % b;
+
+Console.WriteLine(diffrence);
+
+
+Console.WriteLine(sun);
+
+bool isEquel = a == b;
+
+bool isNotEqual = a != b;
+
+bool isGraterThan = a > b;
+
+bool islessThan = a < b;
+
+bool islessThanOrEquel = a <= b;
+
+bool isGreaterThanOrEguel = a >= b;
+
+bool result = (a > 0) && (b > 0);
+
+bool result2 = (a > 0) || (b > 0);
+
+bool norResult = (a > b);
+
+a = a + 5;
+a += 5;
+
+
+
